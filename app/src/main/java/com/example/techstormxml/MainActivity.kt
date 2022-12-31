934f48330038909b9cd48d4b51fd4cf315910610
TaurusSilver002 <anushkamukherjee002@gmail.com>
2022-12-31 21:47:24 +0530

    2nd commit

--- app/src/main/java/com/example/techstormxml/MainActivity.kt
+++ app/src/main/java/com/example/techstormxml/MainActivity.kt
@@ -2,15 +2,18 @@ package com.example.techstormxml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.service.quicksettings.Tile
 import android.view.MenuItem
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.fragment.app.Fragment
 import com.google.android.material.navigation.NavigationView
 import java.nio.channels.AsynchronousFileChannel.open
 
 class MainActivity : AppCompatActivity() {
     lateinit var toggle :ActionBarDrawerToggle
+    lateinit var drawerLayout: DrawerLayout
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -26,9 +29,11 @@ class MainActivity : AppCompatActivity() {
 
         navView.setNavigationItemSelectedListener {
 
+            it.isChecked=true
+
             when(it.itemId){
-                R.id.nav_home -> Toast.makeText(applicationContext,"Clicked Home",Toast.LENGTH_SHORT).show()
-                R.id.nav_schedule -> Toast.makeText(applicationContext,"Clicked Schedule",Toast.LENGTH_SHORT).show()
+                R.id.nav_home -> replaceFragment(HomeFragment(), it.title.toString())
+                R.id.nav_schedule -> replaceFragment(ScheduleFragment(), it.title.toString())
                 R.id.nav_results -> Toast.makeText(applicationContext,"Clicked Result",Toast.LENGTH_SHORT).show()
                 R.id.nav_sponcers -> Toast.makeText(applicationContext,"Clicked Sponcers",Toast.LENGTH_SHORT).show()
                 R.id.nav_team -> Toast.makeText(applicationContext,"Clicked Team",Toast.LENGTH_SHORT).show()
@@ -42,7 +47,15 @@ class MainActivity : AppCompatActivity() {
             true
         }
     }
+private fun replaceFragment(fragment: Fragment,title:String ){
+    val  fragmentManager=supportFragmentManager
+    val fragmentTransaction =fragmentManager.beginTransaction()
+    fragmentTransaction.replace(R.id.fragmentLayout,fragment)
 
+    fragmentTransaction.commit()
+    drawerLayout.closeDrawers()
+    setTitle(title)
+}
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
 
         if(toggle.onOptionsItemSelected(item))
