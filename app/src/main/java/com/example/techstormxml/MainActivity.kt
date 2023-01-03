5ea7332407a22619bc5b9837d92c2e016681ecb1
TaurusSilver002 <anushkamukherjee002@gmail.com>
2023-01-04 00:02:35 +0530

    fragments added

--- app/src/main/java/com/example/techstormxml/MainActivity.kt
+++ app/src/main/java/com/example/techstormxml/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.service.quicksettings.Tile
 import android.view.MenuItem
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.drawerlayout.widget.DrawerLayout
@@ -18,7 +19,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val drawerLayout : DrawerLayout= findViewById(R.id.drawerLayout)
+        drawerLayout  = findViewById(R.id.drawerLayout)
         val navView :NavigationView=findViewById(R.id.Nav_view)
 
         toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
@@ -34,13 +35,13 @@ class MainActivity : AppCompatActivity() {
             when(it.itemId){
                 R.id.nav_home -> replaceFragment(HomeFragment(), it.title.toString())
                 R.id.nav_schedule -> replaceFragment(ScheduleFragment(), it.title.toString())
-                R.id.nav_results -> Toast.makeText(applicationContext,"Clicked Result",Toast.LENGTH_SHORT).show()
-                R.id.nav_sponcers -> Toast.makeText(applicationContext,"Clicked Sponcers",Toast.LENGTH_SHORT).show()
-                R.id.nav_team -> Toast.makeText(applicationContext,"Clicked Team",Toast.LENGTH_SHORT).show()
-                R.id.nav_dev -> Toast.makeText(applicationContext,"Clicked Developers",Toast.LENGTH_SHORT).show()
-                R.id.nav_about_us -> Toast.makeText(applicationContext,"Clicked About Us",Toast.LENGTH_SHORT).show()
-                R.id.nav_announcement -> Toast.makeText(applicationContext,"Clicked Announcemet",Toast.LENGTH_SHORT).show()
-                R.id.nav_photogallery -> Toast.makeText(applicationContext,"Clicked Developers",Toast.LENGTH_SHORT).show()
+                R.id.nav_results -> replaceFragment(ResultsFragment(), it.title.toString())
+                R.id.nav_sponcers -> replaceFragment(SponcerFragment(), it.title.toString())
+                R.id.nav_team -> replaceFragment(TeamFragment(), it.title.toString())
+                R.id.nav_dev -> replaceFragment(DevFragment(), it.title.toString())
+                R.id.nav_about_us -> replaceFragment(AboutUsFragment(), it.title.toString())
+                R.id.nav_announcement -> replaceFragment(AnnouncementFragment(), it.title.toString())
+                R.id.nav_photogallery -> replaceFragment(PhotogalleryFragment(), it.title.toString())
 
 
             }
@@ -50,7 +51,7 @@ class MainActivity : AppCompatActivity() {
 private fun replaceFragment(fragment: Fragment,title:String ){
     val  fragmentManager=supportFragmentManager
     val fragmentTransaction =fragmentManager.beginTransaction()
-    fragmentTransaction.replace(R.id.fragmentLayout,fragment)
+    fragmentTransaction.replace(R.id.frameLayout,fragment)
 
     fragmentTransaction.commit()
     drawerLayout.closeDrawers()
